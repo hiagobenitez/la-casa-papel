8e80324c7a4f4319708128b0bc106f3e3c9c0227
benitezhiago <hiagosantanabenitez@gmail.com>
2022-01-14 15:42:07 -0400

    Chore: Add MetaTags Share

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,6 +43,50 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>La Casa de Papel - Hiago Benitez</title>
+          <meta
+            name="description"
+            content="Site Desenvolvido em React.Js - Next.JS, com intuito de aprofundar a técnicas e conhecimentos avançados em programação, animação e a lib GSAP."
+          />
+          <meta itemProp="name" content="La Casa de Papel - Hiago Benitez" />
+          <meta
+            itemProp="description"
+            content="Site Desenvolvido em React.Js - Next.JS, com intuito de aprofundar a técnicas e conhecimentos avançados em programação, animação e a lib GSAP."
+          />
+          <meta
+            itemProp="image"
+            content="https://www.kindpng.com/picc/m/763-7636587_la-casa-de-papel-icon-hd-png-download.png"
+          />
+
+          <meta property="og:url" content="https://lacasa.hiagobenitez.com" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="La Casa de Papel - Hiago Benitez"
+          />
+          <meta
+            property="og:description"
+            content="Site Desenvolvido em React.Js - Next.JS, com intuito de aprofundar a técnicas e conhecimentos avançados em programação, animação e a lib GSAP."
+          />
+          <meta
+            property="og:image"
+            content="https://www.kindpng.com/picc/m/763-7636587_la-casa-de-papel-icon-hd-png-download.png"
+          />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            content="La Casa de Papel - Hiago Benitez"
+          />
+          <meta
+            name="twitter:description"
+            content="Site Desenvolvido em React.Js - Next.JS, com intuito de aprofundar a técnicas e conhecimentos avançados em programação, animação e a lib GSAP."
+          />
+          <meta
+            name="twitter:image"
+            content="https://www.kindpng.com/picc/m/763-7636587_la-casa-de-papel-icon-hd-png-download.png"
+          />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -69,7 +113,6 @@ export default class MyDocument extends Document {
           <script src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js"></script>
         </Head>
 
-        <title>La Casa de Papel | DEV</title>
         <body>
           <Main />
           <NextScript />
